4e2fc9582d6da62a185e206b1b3407e8e76f5f18
MichaelZalogin <zaloginmiha@gmail.com>
2022-12-09 23:47:53 +0300

    3.0. Тестирование IO [#173905]

--- src/test/java/ru/job4j/io/AnalysisTest.java
+++ src/test/java/ru/job4j/io/AnalysisTest.java
@@ -3,18 +3,15 @@ package ru.job4j.io;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.nio.file.Path;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class AnalysisTest {
 
     @Test
-    void unavailable (@TempDir Path tempDir) throws IOException {
+    void unavailable(@TempDir Path tempDir) throws IOException {
         File source = tempDir.resolve("source.log").toFile();
         try (PrintWriter out = new PrintWriter(source)) {
             out.println("200 10:56:01");
@@ -28,5 +25,12 @@ class AnalysisTest {
             out.println("500 13:59:01");
             out.println("300 15:02:02");
         }
+        File target = tempDir.resolve("target.log").toFile();
+        new Analysis().unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        assertThat("10:57:01;11:02:02;12:59:01;15:02:02;").isEqualTo(rsl.toString());
     }
 }
