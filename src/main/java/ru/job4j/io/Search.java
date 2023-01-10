f8ee3ec66a6349a307970c11aabc44595fded1a4
MichaelZalogin <zaloginmiha@gmail.com>
2023-01-10 21:41:00 +0300

    Сканирование файловой системы. [#106929]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,9 +1,7 @@
 package ru.job4j.io;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -13,7 +11,7 @@ public class Search {
         search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
     }
 
-    public static List<Path> search(Path root, Predicate<Path> condition) {
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
