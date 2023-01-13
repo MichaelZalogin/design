2c274deda8330d9c641ede9d2e2c3454d99347fb
MichaelZalogin <zaloginmiha@gmail.com>
2023-01-13 23:44:05 +0300

    5. Валидация параметров запуска. [#246865 #379841]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -8,18 +8,33 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (args.length < 2) {
-            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
-        }
-        File file = new File(args[0]);
-        if (!file.exists()) {
-            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
-        }
-        if (!file.isDirectory()) {
-            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
-        }
+        validate(args);
         Path start = Paths.get(args[0]);
         search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+    }
+
+    private static void validate(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
+        }
+        validateArg(args);
+    }
+
+    private static void validateArg(String[] args) {
+        File firstArg = new File(args[0]);
+        File secondArg = new File(args[1]);
+        if (!firstArg.exists()) {
+            throw new IllegalArgumentException(
+                    String.format("Not exist %s", firstArg.getAbsoluteFile()));
+        }
+        if (!firstArg.isDirectory()) {
+            throw new IllegalArgumentException(
+                    String.format("Not directory %s", firstArg.getAbsoluteFile()));
+        }
+        if (!secondArg.isFile()) {
+            throw new IllegalArgumentException(
+                    String.format("Second arg %s is not file", secondArg.getAbsoluteFile()));
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
