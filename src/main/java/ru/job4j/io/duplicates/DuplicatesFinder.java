132226d90371fdd78824d822ecbda391a6845351
MichaelZalogin <zaloginmiha@gmail.com>
2023-01-13 23:28:43 +0300

    Поиск дубликатов [#315066 #379842]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -2,21 +2,37 @@ package ru.job4j.io.duplicates;
 
 import java.io.IOException;
 import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DuplicatesFinder {
-    public static void main(String[] args) throws IOException {
-        showDuplicates(Path.of("./"));
+
+    private final Map<FileProperty, ArrayList<Path>> duplicates = new HashMap<>();
+
+    public Map<FileProperty, ArrayList<Path>> getDuplicates() {
+        return this.duplicates;
     }
 
-    public static void showDuplicates(Path file) throws IOException {
+    public static void main(String[] args) throws IOException {
+        DuplicatesFinder duplicatesFinder = new DuplicatesFinder();
+        duplicatesFinder.searchDuplicates(Path.of("./"));
+        duplicatesFinder.showDuplicates();
+    }
+
+    public void searchDuplicates(Path file) throws IOException {
         DuplicatesVisitor duplicatesVisitor = new DuplicatesVisitor();
         Files.walkFileTree(file, duplicatesVisitor);
         duplicatesVisitor.getFilePropertyMap().entrySet().stream()
                 .filter(entry -> entry.getValue().size() > 1)
-                .forEach(entry -> {
-                    System.out.printf("%s - %dB%n", entry.getKey().getName(),
-                            entry.getKey().getSize());
-                    entry.getValue().forEach(System.out::println);
-                });
+                .forEach(entry -> duplicates.put(entry.getKey(), entry.getValue()));
+    }
+
+    public void showDuplicates() {
+        duplicates.entrySet().stream().forEach(entry -> {
+            System.out.printf("%s - %dB%n", entry.getKey().getName(),
+                    entry.getKey().getSize());
+            entry.getValue().forEach(System.out::println);
+        });
     }
 }
