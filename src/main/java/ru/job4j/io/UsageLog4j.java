4b3ef8365f5a1aa92796c53cd9857957ec2d43d7
MichaelZalogin <zaloginmiha@gmail.com>
2023-01-24 02:59:53 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -13,5 +13,22 @@ public class UsageLog4j {
         LOG.info("info message");
         LOG.warn("warn message");
         LOG.error("error message");
+        byte a = 3;
+        short b = 23;
+        int c = 33;
+        long d = 47L;
+        float e = 47.21F;
+        double f = 47.00;
+        char g = 'a';
+        boolean h = true;
+
+        LOG.debug("Name variable: a, value : {}", a);
+        LOG.debug("Name variable: b, value : {}", b);
+        LOG.debug("Name variable: c, value : {}", c);
+        LOG.debug("Name variable: d, value : {}", d);
+        LOG.debug("Name variable: e, value : {}", e);
+        LOG.debug("Name variable: f, value : {}", f);
+        LOG.debug("Name variable: g, value : {}", g);
+        LOG.debug("Name variable: h, value : {}", h);
     }
 }
