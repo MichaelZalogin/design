15fbc0f69972d301f938e7985099ba7b0db6049d
MichaelZalogin <zaloginmiha@gmail.com>
2023-01-26 22:49:07 +0300

    4. JAXB. Преобразование XML в POJO. [#315063]

--- src/main/java/ru/job4j/serialization/Contact.java
+++ src/main/java/ru/job4j/serialization/Contact.java
@@ -1,51 +1,25 @@
 package ru.job4j.serialization;
 
-import java.io.*;
-import java.nio.file.Files;
+import javax.xml.bind.*;
+import javax.xml.bind.annotation.*;
 
-public class Contact implements Serializable {
-    private static final long serialVersionUID = 1L;
-    private final int zipCode;
-    private final String phone;
+@XmlRootElement(name = "Contact")
+public class Contact {
 
-    public Contact(int zipCode, String phone) {
-        this.zipCode = zipCode;
+    @XmlAttribute
+    private String phone;
+
+    public Contact() {
+    }
+
+    public Contact(String phone) {
         this.phone = phone;
-    }
-
-    public int getZipCode() {
-        return zipCode;
-    }
-
-    public String getPhone() {
-        return phone;
     }
 
     @Override
     public String toString() {
         return "Contact{"
-                + "zipCode=" + zipCode
-                + ", phone='" + phone + '\''
+                + "phone='" + phone + '\''
                 + '}';
-    }
-
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        final Contact contact = new Contact(123456, "+7 (111) 111-11-11");
-
-        /** Запись объекта во временный файл, который удалится системой */
-        File tempFile = Files.createTempFile(null, null).toFile();
-        try (FileOutputStream fos = new FileOutputStream(tempFile);
-             ObjectOutputStream oos =
-                     new ObjectOutputStream(fos)) {
-            oos.writeObject(contact);
-        }
-
-        /** Чтение объекта из файла */
-        try (FileInputStream fis = new FileInputStream(tempFile);
-             ObjectInputStream ois =
-                     new ObjectInputStream(fis)) {
-            final Contact contactFromFile = (Contact) ois.readObject();
-            System.out.println(contactFromFile);
-        }
     }
 }
