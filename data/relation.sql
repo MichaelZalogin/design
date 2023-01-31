bcb9052edcbea9d86122421299432531efa1ed6a
MichaelZalogin <zaloginmiha@gmail.com>
2023-01-31 23:26:34 +0300

    0.1. Связи между таблицами [#373615]

--- data/relation.sql
+++ data/relation.sql
@@ -1,44 +1,51 @@
 -- one-to-many
-CREATE TABLE mother (
-id serial PRIMARY KEY,
-name varchar(50),
-last_name varchar(255)
+CREATE TABLE mother
+(
+    id        serial PRIMARY KEY,
+    name      varchar(50),
+    last_name varchar(255)
 );
 
-CREATE TABLE child (
-id serial PRIMARY KEY,
-name varchar(50),
-last_name varchar(255),
-mother_id int REFERENCES mother(id)
+CREATE TABLE child
+(
+    id        serial PRIMARY KEY,
+    name      varchar(50),
+    last_name varchar(255),
+    mother_id int REFERENCES mother (id)
 );
 
 --one-to-one
-CREATE TABLE official_car (
-id serial PRIMARY KEY,
-number char(8)
+CREATE TABLE official_car
+(
+    id     serial PRIMARY KEY,
+    number char(8)
 );
 
-CREATE TABLE employee (
-id serial PRIMARY KEY,
-name varchar(50),
-last_name varchar(255),
-official_car_id int REFERENCES car(id) UNIQUE
+CREATE TABLE employee
+(
+    id              serial PRIMARY KEY,
+    name            varchar(50),
+    last_name       varchar(255),
+    official_car_id int REFERENCES official_car (id) UNIQUE
 );
 
 --many-to-many
-CREATE TABLE book (
-id serial PRIMARY KEY,
-title varchar(50)
+CREATE TABLE book
+(
+    id    serial PRIMARY KEY,
+    title varchar(50)
 );
 
-CREATE TABLE author (
-id serial PRIMARY KEY,
-name varchar(50),
-last_name varchar(255)
+CREATE TABLE author
+(
+    id        serial PRIMARY KEY,
+    name      varchar(50),
+    last_name varchar(255)
 );
 
-CREATE TABLE book_author (
-id_book int REFERENCES book(id),
-id_author int REFERENCES author(id),
-CONSTRAINT book_author_pkey PRIMARY KEY (id_book, id_author)
+CREATE TABLE book_author
+(
+    id_book   int REFERENCES book (id),
+    id_author int REFERENCES author (id),
+    CONSTRAINT book_author_pkey PRIMARY KEY (id_book, id_author)
 );
