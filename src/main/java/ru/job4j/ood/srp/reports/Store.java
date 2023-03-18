dd23a3d43107c9fb9906941268d98f07c422aca5
MichaelZalogin <zaloginmiha@gmail.com>
2023-03-18 03:31:44 +0300

    1. Отчеты. [#850]

---
+++ src/main/java/ru/job4j/ood/srp/reports/Store.java
@@ -0,0 +1,10 @@
+package ru.job4j.ood.srp.reports;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public interface Store {
+    void add(Employee em);
+
+    List<Employee> findBy(Predicate<Employee> filter);
+}
