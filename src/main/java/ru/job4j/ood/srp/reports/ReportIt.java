d812221ed973254440f484cfa42c8bfff02df6ed
MichaelZalogin <zaloginmiha@gmail.com>
2023-03-25 02:29:53 +0300

    Отчеты. [#850]

--- src/main/java/ru/job4j/ood/srp/reports/ReportIt.java
+++ src/main/java/ru/job4j/ood/srp/reports/ReportIt.java
@@ -5,20 +5,24 @@ import java.util.function.Predicate;
 
 public class ReportIt implements Report {
 
-    //    private final DateTimeParser<Calendar> dateTimeParser;
-//
+    private final DateTimeParser<Calendar> dateTimeParser;
+
+    public ReportIt(DateTimeParser<Calendar> dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
+
     @Override
     public String generate(Predicate<Employee> filter, Store store) {
-//        StringBuilder text = new StringBuilder();
-//        text.append("Name; Hired; Fired; Salary;")
-//                .append(System.lineSeparator());
-//        for (Employee employee : store.findBy(filter)) {
-//            text.append(employee.getName()).append(" ")
-//                    .append(dateTimeParser.parse(employee.getHired())).append(" ")
-//                    .append(dateTimeParser.parse(employee.getFired())).append(" ")
-//                    .append(employee.getSalary())
-//                    .append(System.lineSeparator());
-
-        return null;
+        StringBuilder text = new StringBuilder();
+        text.append("Name, Hired, Fired, Salary")
+                .append(System.lineSeparator());
+        for (Employee employee : store.findBy(filter)) {
+            text.append(employee.getName()).append(",")
+                    .append(dateTimeParser.parse(employee.getHired())).append(",")
+                    .append(dateTimeParser.parse(employee.getFired())).append(",")
+                    .append(employee.getSalary())
+                    .append(System.lineSeparator());
+        }
+        return text.toString();
     }
 }
--- src/test/java/ru/job4j/ood/srp/reports/ReportEngineTest.java
+++ src/test/java/ru/job4j/ood/srp/reports/ReportEngineTest.java
@@ -1,27 +1,25 @@
 package ru.job4j.ood.srp.reports;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import ru.job4j.ood.srp.reports.converter.Currency;
 import ru.job4j.ood.srp.reports.converter.CurrencyConverter;
 import ru.job4j.ood.srp.reports.converter.InMemoryCurrencyConverter;
-
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
 import static java.lang.System.*;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReportEngineTest {
 
     Store store = new MemStore();
 
-    List<Employee> employees = new ArrayList<>();
+    List<Employee> employees;
 
     Calendar now = Calendar.getInstance();
+
+    DateTimeParser<Calendar> parser = new ReportDateTimeParser();
 
     @BeforeEach
     void init() {
@@ -34,35 +32,16 @@ public class ReportEngineTest {
         employees = List.of(worker1, worker2, worker3);
     }
 
-    @Disabled
-    @Test
-    public void whenOldGenerated() {
-        MemStore store = new MemStore();
-        Calendar now = Calendar.getInstance();
-        Employee worker = new Employee("Ivan", now, now, 100);
-        DateTimeParser<Calendar> parser = new ReportDateTimeParser();
-        store.add(worker);
-//        Report engine = new ReportEngine(store, parser);
-        StringBuilder expect = new StringBuilder()
-                .append("Name; Hired; Fired; Salary;")
-                .append(System.lineSeparator())
-                .append(worker.getName()).append(" ")
-                .append(parser.parse(worker.getHired())).append(" ")
-                .append(parser.parse(worker.getFired())).append(" ")
-                .append(worker.getSalary())
-                .append(System.lineSeparator());
-//        assertThat(engine.generate(em -> true)).isEqualTo(expect.toString());
-    }
-
     @Test
     public void whenGeneratedItReport() {
-        Report report = new ReportIt();
+        Report report = new ReportIt(parser);
         ReportEngine engine = new ReportEngine(report, store);
-        StringBuilder expect = new StringBuilder()
-                .append("Name; Salary;")
+        StringBuilder expect = new StringBuilder("Name, Hired, Fired, Salary")
                 .append(lineSeparator());
         for (Employee emp : employees) {
-            expect.append(emp.getName()).append(" ")
+            expect.append(emp.getName()).append(",")
+                    .append(parser.parse(emp.getHired())).append(",")
+                    .append(parser.parse(emp.getFired())).append(",")
                     .append(emp.getSalary())
                     .append(lineSeparator());
         }
@@ -87,7 +66,6 @@ public class ReportEngineTest {
     @Test
     public void whenGeneratedAccountingReport() {
         CurrencyConverter currencyConverter = new InMemoryCurrencyConverter();
-        DateTimeParser<Calendar> parser = new ReportDateTimeParser();
         Report report = new ReportAccounting(parser, currencyConverter, Currency.RUB, Currency.EUR);
         ReportEngine engine = new ReportEngine(report, store);
         StringBuilder expect = new StringBuilder("Name; Hired; Fired; Salary;")
