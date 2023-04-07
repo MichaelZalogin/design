b7260b89d89a6f097e32e7f042f0f528bb6ba9ed
MichaelZalogin <zaloginmiha@gmail.com>
2023-04-08 02:48:45 +0300

    Аннотации lombok [#504889]

---
+++ src/main/java/ru/job4j/model/Category.java
@@ -0,0 +1,15 @@
+package ru.job4j.model;
+
+import lombok.*;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@RequiredArgsConstructor
+public class Category {
+    @Getter
+    @EqualsAndHashCode.Include
+    @NonNull
+    private int id;
+    @Getter
+    @Setter
+    private String name;
+}
